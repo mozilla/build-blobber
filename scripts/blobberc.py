74fd32b95c60592ed49b3e679b0a25dc2f7ef1d6
Mihai Tabara <tabara.mihai@gmail.com>
2013-07-16 10:48:30 -0500

    Add branch as command line argument.

--- scripts/blobberc.py
+++ scripts/blobberc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-"""Usage: blobberc.py -u URL... [-a] [-v] FILE...
+"""Usage: blobberc.py -u URL... [-b BRANCH] [-v] FILE...
 
 -u, --url URL          URL to blobber server to upload to. Multiple URLs can be
                        specified, and they will be tried in random order
--a, --unpack-archives  Unpack archives (e.g. .zip, .tar.gz) before uploading
+-b, --branch BRANCH    Specify branch for the file (e.g. try, mozilla-central)
 -v, --verbose          Increase verbosity
 
 FILE                   Local file(s) to upload
@@ -26,18 +26,10 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def has_blob(urls, hashalgo, blobhash):
-    url = urlparse.urljoin(urls[0], '/blobs/{}/{}'.format(hashalgo, blobhash))
-    response = requests.head(url)
-    return response.ok
-
-
-def upload_file(urls, filename, hashalgo='sha1', blobhash=None, check_first=False):
+def upload_file(urls, filename, hashalgo='sha1', blobhash=None,
+                branch='mozilla-inbound'):
     if blobhash is None:
         blobhash = blobber.hashes.filehash(filename, hashalgo)
-
-    if check_first and has_blob(urls, hashalgo, blobhash):
-        return blobhash
 
     url = urlparse.urljoin(urls[0], '/blobs/{}/{}'.format(hashalgo, blobhash))
 
@@ -47,12 +39,11 @@ def upload_file(urls, filename, hashalgo='sha1', blobhash=None, check_first=Fals
         'data': open(filename, 'rb'),
         'filename': filename,
         'filesize': os.path.getsize(filename),
-        'branch': 'branch-input',
+        'branch': branch,
         'mimetype': 'application/octet-stream',
     })
     req = urllib2.Request(url, datagen, headers)
     urllib2.urlopen(req)
-
 
 
 def main():
@@ -67,13 +58,16 @@ def main():
     else:
         loglevel = logging.INFO
 
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=loglevel)
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s",
+                        level=loglevel)
     logging.getLogger('requests').setLevel(logging.WARN)
-
 
     for f in args['FILE']:
         if os.path.isfile(f):
-            blob_id = upload_file(args['--url'], f, check_first=False)
+            if args['--branch']:
+                blob_id = upload_file(args['--url'], f, branch=args['--branch'])
+            else:
+                blob_id = upload_file(args['--url'], f)
             print f, blob_id
 
 if __name__ == '__main__':
